49b8a112d394ac6c7c6ef1d3f79521f334ca7986
ethanholleman <etholleman@ucdavis.edu>
2022-05-20 12:12:51 -0700

    add outline of R plotting script

---
+++ scripts/plotT2C.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(RColorBrewer)
+library(ggpubr)
+
+
+# read concat tsv file here
+
+
+# violin plots of replicates over conditions
+
+
+# Line plot of conversion frequency all samples
+
+
+# write plots to png / pdf formats 
